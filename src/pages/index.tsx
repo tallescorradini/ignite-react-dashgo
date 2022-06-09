c3a073163424d8db267521f30f01d742e4b628e4
Talles Corradini <tallescorradini@gmail.com>
2022-06-09 19:48:25 -0300

    refactor: remove boilerplate and add typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Dashgo</title>
+      </Head>
+
+      <h1>Dashgo</h1>
+    </div>
+  );
+}
