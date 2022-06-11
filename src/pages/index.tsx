b687e385b9fe368d8793f65aa4174b3ca2f94bee
Talles Corradini <tallescorradini@gmail.com>
2022-06-10 21:15:01 -0300

    feat: add login form components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,62 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
       <Head>
         <title>Dashgo</title>
       </Head>
 
-      <h1>Dashgo</h1>
-    </div>
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+
+          <Button type="submit" mt="6" colorScheme="pink" size="lg">
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
   );
 }
